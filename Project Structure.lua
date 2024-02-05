c778766524d49e40e1484fb1bad1a2986386d526
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-02-05 23:56:32 +0530

    Create Project Structure.lua

---
+++ Project Structure.lua
@@ -0,0 +1,7 @@
+MaxSubarraySumProject/
+|-- src/
+|   |-- Main.java
+|   |-- MaxSubarraySumGUI.java
+|-- lib/
+|   |-- mysql-connector-java-<version>.jar  (MySQL Connector/J JAR file)
+|-- MaxSubarraySumDatabase.sql
